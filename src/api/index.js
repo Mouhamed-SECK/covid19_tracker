b9c74080e7b69323d452221e5cc7e47961484ff7
Mouhamed27-99 <medsoseck@gmail.com>
2020-04-11 17:26:14 +0200

    Fetching daily data from api and adding a chart line line width data to Chart Component

--- src/api/index.js
+++ src/api/index.js
@@ -21,6 +21,13 @@ export const fetchData = async () => {
 
 export const fetchDailyData = async () => {
   try {
-    const { data } = await axios.get(`${url}\daily`);
+    const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifiedData;
   } catch (error) {}
 };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { fetchDailyData } from "./../../api/index";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
+
 const Chart = () => {
-  const [dailyData, setDailyData] = useEffect({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -9,8 +13,33 @@ const Chart = () => {
     };
 
     fetchApi();
-  });
-  return <div></div>;
+    // console.log(dailyData);
+  }, []);
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0 , 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
